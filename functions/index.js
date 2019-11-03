5929940ff1692c556d2e72e40b76cb7214ed77ce
Matthew Guo <mg2239@cornell.edu>
2019-11-03 13:48:37 -0500

    added endpoint to add a purchase and get item's data by id

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
 
 admin.initializeApp()
@@ -9,13 +10,40 @@ const db = admin.firestore()
 const products = db.collection('products')
 const users = db.collection('users')
 
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
+
 exports.api = functions.https.onRequest(app)
 
 app.get('/getProducts', async (req, res) => {
   let prod_snapshot = await products.get()
   let prod_list = []
   prod_snapshot.forEach((doc) => {
-    prod_list.push(Object.assign({}, { id: doc.id }, doc.data()))
+    prod_list.push(doc.data())
   })
   res.send({ data: prod_list })
+})
+
+app.get('/:product_id', (req, res) => {
+  let { product_id } = req.params
+  products.doc(product_id).get()
+    .then(doc => {
+      res.send({ data: doc.data() })
+      return
+    })
+    .catch(error =>
+      res.send(error)
+    )
+})
+
+app.post('/:product_id/addPurchase', (req, res) => {
+  try {
+    let { product_id } = req.params
+    let { name, netid, venmo_id, color, size } = req.body
+    let user = { name, netid, venmo_id, product_id, color, size }
+    users.doc(netid).set(user)
+    res.send({ data: user })
+  } catch (error) {
+    res.send({ error })
+  }
 })
